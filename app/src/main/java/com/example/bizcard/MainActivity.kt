0863210b3996b9789dffa2962f65ece5fb7c091e
viradhanus <e16083@eng.pdn.ac.lk>
2022-01-07 16:08:13 +0530

    finalize

--- app/src/main/java/com/example/bizcard/MainActivity.kt
+++ app/src/main/java/com/example/bizcard/MainActivity.kt
@@ -6,17 +6,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.bizcard.ui.theme.BizCardTheme
@@ -37,7 +45,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
-
+    val buttonClickdState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,14 +74,22 @@ fun CreateBizCard() {
                 CreateInfo()
                 Button(
                     onClick = {
-                        Log.d("Clicked", "created card")
+                        buttonClickdState.value = !buttonClickdState.value
 
                     }) {
                     Text(
-                        text = "portfolio",
+                        text = "Portfolio",
                         style = MaterialTheme.typography.button
                     )
 
+                }
+                if (buttonClickdState.value) {
+                    Content()
+
+                } else {
+                    Box() {
+
+                    }
                 }
             }
         }
@@ -100,9 +118,9 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateProfileImage() {
+private fun CreateProfileImage(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(),
         shape = CircleShape,
@@ -113,7 +131,7 @@ private fun CreateProfileImage() {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
-            modifier = Modifier.size(
+            modifier = modifier.size(
                 135.dp
             ),
             contentScale = ContentScale.Crop,
@@ -162,5 +180,30 @@ fun Content() {
 
 @Composable
 fun Portfolio(data: List<String>) {
-    Text(text = "Project go here!")
+    LazyColumn {
+        items(data) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape
+
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(16.dp)
+                ) {
+                    CreateProfileImage(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier
+                        .padding(7.dp)
+                        .align(alignment = Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A cool project", style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
 }
